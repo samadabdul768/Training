48a600a56501f714ef80700138f939799dcf162f
samadabdul768 <abdulabsolutetravel000@gmail.com>
2025-06-11 16:23:53 +0530

    Initial commit

---
+++ src/app/form/page.jsx
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+function FormPage() {
+  return (
+    <div className="bg-white flex flex-col justify-center items-center h-96 w-80 rounded-4xl">
+      <h1 className= {` ${inter.className}mb-5 text-2xl font`}>Sign In With</h1>
+      <form>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div>
+            <label className="flex flex-col">Username</label>
+            <input type="text" placeholder="Username"  className="border-2"/>
+          </div>
+          <div>
+            <label className="flex flex-col">Password</label>
+            <input type="password" placeholder="Password" className="border-2"/>
+          </div>
+          <button className="bg-black color-white text-white w-full rounded-2xl">Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default FormPage;
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import FormPage from "./form/page";
+
+export default function Home() {
+  return (
+   <div className="bg-gradient-to-r from-blue-500 to-purple-500 h-screen w-full flex justify-center items-center">
+    
+    <FormPage/>
+   </div>
+  )
+}
